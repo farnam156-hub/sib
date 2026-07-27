7975e9b4670f3b38e22c5e6b09a32599856d7168
farnam156-hub <farnam156@gmail.com>
2026-07-27 06:36:17 +0330

    Update script.js

--- script.js
+++ script.js
@@ -64,6 +64,8 @@ const galleryItems = [
     { src: 'https://s21.uupload.ir/files/chaybagh/IMG_20260724_222449.jpg', category: 'places' },
     { src: 'https://s25.uupload.ir/files/chaybagh/IMG_20260724_222432.jpg', category: 'places' },
     { src: 'https://s25.uupload.ir/files/chaybagh/IMG_20260724_222353.jpg', category: 'places' },
+    { src: 'https://s25.uupload.ir/files/chaybagh/IMG_20260724_222341.jpg', category: 'places' }.
+    { src: 'https://s25.uupload.ir/files/chaybagh/IMG_20260724_222327.jpg', category: 'places' }.
 ];
 
 const galleryGrid = document.getElementById('galleryGrid');
